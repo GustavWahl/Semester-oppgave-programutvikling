b2308706e04c15ceb250a5bbf726ac06eceafb20
Gustav wehn <36506962+GWehn@users.noreply.github.com>
2018-04-27 17:33:15 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
@@ -24,13 +25,22 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-
+import javafx.event.ActionEvent;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.layout.VBox;
 
 import static javafx.scene.paint.Color.BLACK;
 import static javafx.scene.paint.Color.RED;
-import static javafx.scene.paint.Color.color;
+import javafx.stage.Screen;
 
 public class Main extends Application{
+    
+    private enum STATUS{
+        SPILL,
+        MENY
+    }
+    
+    private STATUS Status = STATUS.SPILL;
 
     private Pane root;
     private Spiller player;
@@ -38,21 +48,14 @@ public class Main extends Application{
     private GameObject wall2;
     private GameObject wall3;
     private GameObject wall4;
-    private boolean powerup;
-    private  GameObject up;
-
-
-
+    private Button strt;
 
     List<Fiender> fiender = new ArrayList<>();
     List<Skudd> bullets = new ArrayList<>();
     List<GameObject> walls = new ArrayList<>();
-    List<GameObject> powerups = new ArrayList<>();
     List<FiendeSkudd> Enemybullets = new ArrayList<>();
 
-
-
-    public static void main(String[] args) {
+    public static void Main(String[] args) {
 
         launch(args);
     }
@@ -79,44 +82,41 @@ public class Main extends Application{
 
     public boolean equalsX(GameObject gameObject, Point2D point2D){
 
-
         return gameObject.getVelocity().getX() == point2D.getX();
     }
 
     public boolean equalsY(GameObject gameObject, Point2D point2D){
 
-
         return gameObject.getVelocity().getY() == point2D.getY();
     }
 
-
-
-
-
-
-
-
+    
     // Denne metoden Viser og lager alt innenfor Scenen
     private Parent createContent(){
         root = new Pane();
-        root.setPrefSize(2400,1200);
+        
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        double width = screenBounds.getWidth();
+        double height = screenBounds.getHeight();
+        
+        root.setPrefSize(height, width);
 
         player = new Spiller(100, 10, 0,"Gustav",lagkarakter());
         player.setVelocity(new Point2D(0,-0.001));
 
-// (new Rectangle(20,20,Color.BLUE)),(new Circle(5,5,5,(new Color(0.1f,0.3f,1.0f, 1.0))))
+        // (new Rectangle(20,20,Color.BLUE)),(new Circle(5,5,5,(new Color(0.1f,0.3f,1.0f, 1.0))))
 
         addGameObject(player, 300, 300);
 
-        wall1 = new GameObject(new Rectangle(2400,10,new Color(0, 0, 0, 0.4824)));
-        wall2 = new GameObject(new Rectangle(2400,10,new Color(0, 0, 0, 0.4824)));
-        wall3 = new GameObject(new Rectangle(10,1200,new Color(0, 0, 0, 0.4824)));
-        wall4 = new GameObject(new Rectangle(10,1200,new Color(0, 0, 0, 0.4824)));
+        wall1 = new GameObject(new Rectangle(width,10,new Color(0, 0, 0, 0.4824)));
+        wall2 = new GameObject(new Rectangle(width,10,new Color(0, 0, 0, 0.4824)));
+        wall3 = new GameObject(new Rectangle(10,height,new Color(0, 0, 0, 0.4824)));
+        wall4 = new GameObject(new Rectangle(10,height,new Color(0, 0, 0, 0.4824)));
 
         addGameObject(wall1,0,0);
-        addGameObject(wall2,0,1190);
+        addGameObject(wall2,0,height - 30);
         addGameObject(wall3,0,0);
-        addGameObject(wall4,2390,0);
+        addGameObject(wall4,width - 10,0);
 
         walls.add(wall1);
         walls.add(wall2);
@@ -129,13 +129,39 @@ public class Main extends Application{
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                onUpdate();
+                if(Status == STATUS.SPILL){
+                    onUpdate();
+                }
             }
         };
         timer.start();
 
         return root;
-
+    }
+    
+    public Parent createMeny(){
+        strt = new Button("Start Nytt Spill");
+        
+        
+        javafx.scene.control.Button hScore = new javafx.scene.control.Button();
+        hScore.setText("High Score");
+        hScore.setOnAction((ActionEvent event) -> {
+            System.out.println("Laster High");
+        });
+        
+        javafx.scene.control.Button load = new javafx.scene.control.Button();
+        load.setText("Load");
+        load.setOnAction((ActionEvent event) -> {
+            System.out.println("Laster");
+        });
+        
+        VBox root = new VBox();
+        root.setSpacing(30);
+        root.setAlignment(Pos.CENTER);
+        
+        root.getChildren().addAll(strt, hScore, load);
+        
+        return root;
     }
 
 
@@ -143,7 +169,6 @@ public class Main extends Application{
     private void addBullet(Skudd bullet, double x, double y){
         bullets.add(bullet);
         addGameObject(bullet,x,y);
-
     }
 
 
@@ -153,22 +178,12 @@ public class Main extends Application{
         fiender.add(fiende);
         addGameObject(fiende,x,y);
 
-
         System.out.println(fiende.getDecideActiveState());
-
     }
 
     private void addFiendeBullet(FiendeSkudd bullet, double x, double y){
         Enemybullets.add(bullet);
         addGameObject(bullet,x,y);
-
-    }
-
-
-    private void addPowerUp(GameObject ups, double x, double y){
-        powerups.add(ups);
-        addGameObject(ups,x,y);
-
     }
 
 
@@ -206,11 +221,9 @@ public class Main extends Application{
 
                         }catch (Exception e){
 
-                        }
-*/
+                        }*/
+                     
                         root.getChildren().remove(fiende.getView());
-
-
                     }
                 }
 
@@ -232,10 +245,7 @@ public class Main extends Application{
         }
 
         for (Fiender fiende3 : fiender) {
-
-
             for (FiendeSkudd fbullet : Enemybullets) {
-
                 if (fbullet.isColliding(player)) {
                     player.setHp(player.getHp() - 10);
                     fbullet.setAlive(false);
@@ -243,6 +253,7 @@ public class Main extends Application{
                     fbullet.update();
                 }
                 if (fbullet.isColliding(wall1) || fbullet.isColliding(wall2) || fbullet.isColliding(wall3) || fbullet.isColliding(wall4)) {
+                    // bullet.setVelocity(new Point2D(Math.random()*10-5,Math.random()*10-5));
                     if (Math.random() * 13 - 5 < 4) {
                         fbullet.setVelocity(new Point2D(fbullet.getVelocity().getX() + Math.random() * 10 - 5, fbullet.getVelocity().getY() + Math.random() * 10 - 5).multiply(-1));
                     } else {
@@ -263,24 +274,18 @@ public class Main extends Application{
 
                     fiendeBullet2.update();
                 }
-
-
             }
 
 
                     fiende3.FSM(player, fiende3);
                 //   fiende3.update();
-
-
-
-
         }
 
 
         for (Fiender fiende1 : fiender) {
             if (fiende1.isColliding(player) || fiende1.isColliding(wall1) || fiende1.isColliding(wall2) || fiende1.isColliding(wall3) || fiende1.isColliding(wall4)) {
-
-                fiende1.setVelocity(fiende1.getVelocity().multiply(-5));
+                // fiende1.setVelocity(new Point2D(Math.random()*4-2,Math.random()*4-2));
+                fiende1.setVelocity(new Point2D(fiende1.getVelocity().getX() + Math.random() * 2 - 1, fiende1.getVelocity().getY() + Math.random() * 2 - 1).multiply(-1));
 
 
 
@@ -292,8 +297,6 @@ public class Main extends Application{
                     System.out.println("Game Over");
                 }
             }
-
-
         }
 
 
@@ -318,23 +321,6 @@ public class Main extends Application{
 
         }
 
-
-        if (Math.random() < 0.005){
-
-            addPowerUp(  new GameObject(new Circle(5,5,5,Color.BLACK)), Math.random()*600, 600);
-
-        }
-        for (GameObject x:powerups) {
-            if (x.isColliding(player)){
-
-                powerup = true;
-                root.getChildren().remove(x.getView());
-            }
-
-        }
-
-
-
 //_________________________TESTING OF METHODS_______________________//
 
         // testing av hp system
@@ -345,56 +331,54 @@ public class Main extends Application{
           System.out.println( Math.pow((player.getX() - (fiende5.getX())),2)*0.001);
            System.out.println( Math.pow((player.getY() - (fiende5.getY())),2)*0.001);
         }*/
-
-
     }
-
-
-
-
-
-
-
 
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        
+        stage.setScene(new Scene(createMeny()));
+        
         // Dette er scenen, vi puter inn createcontent i setScene.
+        strt.setOnAction((ActionEvent event) -> {
+            stage.setScene(new Scene(createContent()));
+        
+        
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 
-
-
-        stage.setScene(new Scene(createContent()));
+        //set Stage boundaries to visible bounds of the main screen
+        stage.setX(primaryScreenBounds.getMinX());
+        stage.setY(primaryScreenBounds.getMinY());
+        stage.setWidth(primaryScreenBounds.getWidth());
+        stage.setHeight(primaryScreenBounds.getHeight());
+        
         stage.getScene().setOnKeyPressed((KeyEvent e) ->{
-
-
-            //dette er bevegelse , Point2D blir brukt dette er en farts vektor og vi har x, -x, y, -y
+            
+            if(Status == STATUS.SPILL){
+            
+            if (null != e.getCode()) //dette er bevegelse , Point2D blir brukt dette er en farts vektor og vi har x, -x, y, -y
             //Keycode er satt opp mot en lambda metode som gjør at når man presser en tast eller releaser den(nedenfor) så gjøres det en handling
-
-            if (e.getCode() == KeyCode.A){
-                player.setVelocity(new Point2D(-5,0));
-
-
-            }
-
-            else if (e.getCode() == KeyCode.D){
-                player.setVelocity(new Point2D(5,0));
-
-
-            }
-            else if (e.getCode() == KeyCode.W){
-                player.setVelocity(new Point2D(0,-5));
-
-            }
-            else if (e.getCode() == KeyCode.S){
-                player.setVelocity(new Point2D(0,5));
-
-            }
-            else if (e.getCode() == KeyCode.Q){
-                player.rotateLeft();
-            }
-            else if (e.getCode() == KeyCode.E){
-                player.rotateRight();
+            switch (e.getCode()) {
+                case A:
+                    player.setVelocity(new Point2D(-5,0));
+                    break;
+                case D:
+                    player.setVelocity(new Point2D(5,0));
+                    break;
+                case W:
+                    player.setVelocity(new Point2D(0,-5));
+                    break;
+                case S:
+                    player.setVelocity(new Point2D(0,5));
+                    break;
+                case Q:
+                    player.rotateLeft();
+                    break;
+                case E:
+                    player.rotateRight();
+                    break;
+                default:
+                    break;
             }
 
 
@@ -402,11 +386,8 @@ public class Main extends Application{
 
                 Skudd bullet = new Skudd();
 
-
                 // POWERUPS , skriv in true ini der så kan man aktivere de, denne gjør at du skyter 4 kuler ekstra
-                if(powerup){
-
-
+                if(true){
                     if (equalsX(player, new Point2D(0.1,0)) || equalsX(player,new Point2D(-0.1,0))){
                         Skudd bullet1 = new Skudd();
                         Skudd bullet2 = new Skudd();
@@ -420,9 +401,6 @@ public class Main extends Application{
                         bullet4.setVelocity(player.getVelocity().add(0, -0.02).normalize().multiply(10));
                         addBullet(bullet3, player.getView().getTranslateX(), player.getView().getTranslateY());
                         addBullet(bullet4, player.getView().getTranslateX(), player.getView().getTranslateY());
-
-
-
 
                     } else if (equalsY(player, new Point2D(0,0.1)) || equalsY(player,new Point2D(0,-0.1))){
 
@@ -439,28 +417,24 @@ public class Main extends Application{
                         addBullet(bullet3, player.getView().getTranslateX(), player.getView().getTranslateY());
                         addBullet(bullet4, player.getView().getTranslateX(), player.getView().getTranslateY());
                     }
-
-
-
-
                 }
-
-
-
-
-
 
                 bullet.setVelocity(player.getVelocity().normalize().multiply(10));
                 addBullet(bullet, player.getView().getTranslateX(), player.getView().getTranslateY());
 
-
                 // Også Powerup
                 if(false){
                     bullet.setVelocity(player.getVelocity().normalize().multiply(30));
-
-
                 }
-
+            }
+            }
+            
+            if(e.getCode() == KeyCode.P){
+                if(Status == STATUS.SPILL){
+                    Status = STATUS.MENY;
+                }else{
+                    Status = STATUS.SPILL;
+                }
             }
 
             //_______________________TESTING AV VELOCITY_____________________//
@@ -470,56 +444,37 @@ public class Main extends Application{
             System.out.println(player.getVelocity().getX());
             System.out.println(player.getVelocity().getY());
             //System.out.println(new Point2D(0,5.0).getY());
-*/
-
-
+        */
         });
 
         /*fordi fartsvektoren aldri stopper etter du har presset den , må man lage en keyrelease som setter farten din ned til nærmere 0 verdi
             Det er putta 0.1 som verdi fordi skuddene må vite hvilken retning man står mot, når skuddene blir skutt ut ganges de opp sånn at de går mye raskere.
 
-*/
+        */
 
         stage.getScene().setOnKeyReleased(e ->{
-
-
-            if (e.getCode() == KeyCode.A){
-                player.setVelocity(new Point2D(-0.1,0));
-
+            
+            if(Status == STATUS.SPILL){
+            if (null != e.getCode())switch (e.getCode()) {
+                case A:
+                    player.setVelocity(new Point2D(-0.1,0));
+                    break;
+                case D:
+                    player.setVelocity(new Point2D(0.1,0));
+                    break;
+                case W:
+                    player.setVelocity(new Point2D(0,-0.1));
+                    break;
+                case S:
+                    player.setVelocity(new Point2D(0,0.1));
+                    break;
+                default:
+                    break;
             }
-
-            else if (e.getCode() == KeyCode.D){
-                player.setVelocity(new Point2D(0.1,0));
-
-
             }
-            else if (e.getCode() == KeyCode.W){
-                player.setVelocity(new Point2D(0,-0.1));
-
-
-            }
-            else if (e.getCode() == KeyCode.S){
-                player.setVelocity(new Point2D(0,0.1));
-
-
-
-            }
-
-
-
-
+        });
         });
 
-
-
-
-
-
         stage.show();
-
     }
-
-
-
-
 }
